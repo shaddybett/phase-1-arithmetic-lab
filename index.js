4917d2d45f5a245339ba846bcaa21c404c146948
Shadrack kipkorir <shadybett540@gmail.com>
2023-09-25 16:10:20 +0300

    complet

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+let num1=2
+let num2=31
+let multiply =num1*num2
+// Write your code here 
+let random = Math.floor(Math.random() * 8)+ 1;
+let num3 = 16
+let num4 = 4
+let mod = num3/num4
+let max =20
